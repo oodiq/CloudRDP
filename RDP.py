519cb9b4a7e04ab9767ae93c94e686b016ec8966
Shodiqul Irwan <98737613+oodiq@users.noreply.github.com>
2022-03-04 03:51:35 +0800

    Add files via upload

--- RDP.py
+++ RDP.py
@@ -58,9 +58,8 @@ class RDP:
         l.start()
 
         system("export DEBIAN_FRONTEND=noninteractive")
-        system("sudo apt install -y gnome &> /dev/null")
-        system("sudo apt install --assume-yes --fix-broken &> /dev/null")
-        system("sudo bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/gnome-session\" > /etc/chrome-remote-desktop-session'")
+        system("sudo apt install -y mate-desktop-environment &> /dev/null")
+        system("sudo bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/mate-session\" > /etc/chrome-remote-desktop-session'")
         system("sudo apt remove --assume-yes gnome-terminal &> /dev/null")
         system("sudo apt install --assume-yes xscreensaver &> /dev/null")
         system("systemctl disable lightdm.service &> /dev/null")
@@ -71,7 +70,7 @@ class RDP:
     def install_firefox():
         l = Loading("Firefox")
         l.start()
- 
+
         system("sudo apt install -y firefox-esr &> /dev/null")
 
         l.stop()
